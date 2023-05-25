0918eb1ea5d3991948064592c58cc585d3956c90
Onuchukwu Adebayo <adebayop.o@gmail.com>
2023-05-25 23:52:51 +0100

    feat: connected to the api

--- src/adapters/clients/graphql/client.ts
+++ src/adapters/clients/graphql/client.ts
@@ -1,7 +1,8 @@
+import config from "@/adapters/config";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://127.0.0.1:8000/graphql",
+  uri: config.GRAPHQL_API_URL,
   cache: new InMemoryCache(),
 });
 
